f69783881672f9ecbe6aa21e32bdcff48310ecc1
Leila Sarbazi <leila.sarbazi@adesso.de>
2023-12-28 18:42:11 +0100

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,6 @@
-def call(Map config = [:]){
-    script {
-        env.CONTAINER_FILE_PATH = config.containerFilePath == null ? "Dockerfile" : config.containerFilePath
-    }
+def call(Map config = [containerFilePath:"Containerfile"]){
+        env.CONTAINER_FILE_PATH = config.containerFilePath
 
-    println "${BUILD_VERSION} and the ${CONTAINER_FILE_PATH}"
+    println "${CONTAINER_FILE_PATH}"
     println "${config.containerFilePath}"
-    println "${IMAGE_REPO} and the ${PROJECT}"
 }
