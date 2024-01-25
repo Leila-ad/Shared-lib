6a48f2c16f171ba4e49e8bf3935f0701bdbd3313
Leila Sarbazi <leila.sarbazi@adesso.de>
2024-01-25 11:43:37 +0100

    Update consoleLogs.groovy

--- vars/consoleLogs.groovy
+++ vars/consoleLogs.groovy
@@ -8,7 +8,7 @@ def call(Map config = [:]) {
     GIT_SHORT_HASH : [${GITSHORTHASH}]
     TIMESTAMP      : [${TIMESTAMP}]
     ${config.collect({key, value -> "$key: [$value]\n"})}
-    ${config.split(entry -> entry)}
+    ${config.split({entry -> entry})}
     """
   }
 }
