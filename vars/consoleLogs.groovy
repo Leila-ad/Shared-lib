7d50454ca1e157609b3a68fa5d89a14059343c0e
Leila Sarbazi <leila.sarbazi@adesso.de>
2024-01-25 10:52:21 +0100

    Update consoleLogs.groovy

--- vars/consoleLogs.groovy
+++ vars/consoleLogs.groovy
@@ -3,14 +3,12 @@ def call(Map config = [:]) {
   GITSHORTHASH  = getGitShortHash()
   TIMESTAMP     = "21.12.2023"
   
-  def arr = ["TIMESTAMP: [${TIMESTAMP}]", "GIT SHORT HASH: [${GITSHORTHASH}]"]
-  // config.each{key, value ->
-  //   arr.append("$key: [$value]")
-  // }
-  
-  sh """
-    for a in arr; do
-        echo "$a"
-    done
-  """
+  Map test =[
+    GIT_SHORT_HASH = $GITSHORTHASH
+    TIMESTAMP      = $TIMESTAMP
+  ]
+
+  script {
+    echo "${test}"
+  }
 }
