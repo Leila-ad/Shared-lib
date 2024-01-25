0411ab95f0785dd422e28f6b8c5584ced54e8569
Leila Sarbazi <leila.sarbazi@adesso.de>
2024-01-25 09:52:25 +0100

    Update consoleLogs.groovy

--- vars/consoleLogs.groovy
+++ vars/consoleLogs.groovy
@@ -4,15 +4,10 @@ def call(Map config = [:]) {
   TIMESTAMP     = "21.12.2023"
 
   script {
-    def arr = config.each{key, value -> 
-      "$key: [$value]"
-    }
     echo """
       TIMESTAMP: [${TIMESTAMP}]
       GIT SHORT HASH: [${GITSHORTHASH}]
-      ${arr}
       ${config.each{key, value -> "$key: [$value]"}}
-      $(config.each{key, value -> "$key: [$value]"})
     """
   }
 }
